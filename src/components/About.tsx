9e7da8314b21827f010412079b892a74460c0572
Johnfil Initan <njohnfil.pr+github@gmail.com>
2023-12-11 22:08:54 +0800

    Light mode fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,23 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section id="about" className='flex flex-col gap-10 items-center z-50 px-4 sm:px-24'>
+    <section id="about" className='flex flex-col gap-10 items-center z-50 px-4 sm:px-10'>
       <h1 className='text-3xl'>About Me</h1>
-      <div className='w-full md:w-4/5 lg:w-2/3 px-0 text-sm sm:text-xs md:text-base lg:text-lg text-center leading-relaxed font-extralight'>
-        After I finish my degree in Computer Engineering in the University of San Carlos, I plan to continue working in computer-related industries. I only started delving into the world of code when I entered college, but all the concepts, principles and knowledge I&apos;ve accumulated over the years in terms of the design, planning, and development of software and hardware applications has really pushed me to learn more and apply these learnt skills in useful scenarios. 
+      <div className='w-full md:w-4/5 xl:w-2/5 px-0 text-sm sm:text-xs md:text-base lg:text-lg text-center font-extralight'>
+
+        After I get my Computer Engineering degree from the University of San Carlos, I want to keep working with computers. I only started learning to code in college, but I&apos;ve picked up a lot about designing and building software and hardware. I began with C, which gave me a strong base for understanding how to code and work with data. Learning Java taught me a different way to program called Object-Oriented Programming and helped me with web development.
         <br></br>
         <br></br>
-        My roots in programming started with C, and I believe starting my coding journey with this language gave me a strong foundation in programming concepts and data structures that are usually abstracted in higher-level languages. Learning Java also introduced me to a different programming style from procedural languages, which was Object-Oriented Programming; I learned concepts such as abstraction, encapsulation, and inheritance, as well as classes and objects which became really useful when learning web development.
+        In my third year, I started making websites using frameworks like React and NextJS. Within a year, I could build websites and understand JavaScript better. Web development was a big step for me because I learned about things like databases, queries, APIs, and how to make websites secure. I worked with a team of developers, and we used tools like Figma and Git, plus a method called Scrum for managing projects. Our college thesis also helped me learn more about researching and introduced me to machine learning and artificial intelligence using Python and tools like Pandas, NumPy, and TensorFlow. Even though our thesis is done, my interest in artificial intelligence is still strong, and I&apos;m excited to keep exploring the world of coding.
         <br></br>
         <br></br>
-        I didn&apos;t really get into website building until my third year of programming since it was just an optional track in the course. However, within just a year of starting, I was already able to build websites with frameworks such as React and NextJS. In fact, it was making websites that really gave me a deeper understanding of JavaScript. Web development was the biggest leap in my tech journey as I was able to learn technologies outside of programming, including: databases with PostgreSQL, ORMs, HTTPS APIs, the RESTful framework, authentication, SEOs and many more. Taking in all these knowledge in such a short amount of time was initially difficult, but there was also a sense of satisfaction that came with it. Of course, I could never have done all of these without my fellow team of developers who shares the same passion for web development that I do. Collaborating with them gave me experience on how programming in a team was like, as well as teaching me tools like Figma, Git, Trello, and other project management concepts like Scrum.
-        <br></br>
-        <br></br>
-        Our college thesis was a great endeavour that further honed my researching capabilities. Working on our thesis also gave me a glimpse on machine learning, artificial intelligence, and data analysis. It gave me a stronger foundation on Python and the many libraries that come with it: there was Pandas, NumPy, MatPlot for data analysis; and TensorFlow, Keras and SciKit Learn for artificial intelligence. Although we&apos;ve already finished our thesis, my interest in articial intelligence has only intensified. Beyond this, I can only hope that my passion for technology does not burn out and that I can further explore deeper into the world of code.
-        <br></br>
-        <br></br>   
         <p className="flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">printf(&quot;Hello World!&quot;)</code>
+          <code className="font-mono font-bold">printf(&quot;Hello World!&quot;);</code>
         </p>
       </div>
     </section>
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -6,7 +6,7 @@ import { GoPaperclip } from "react-icons/go";
 
 const Links = () => {
   return (
-      <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="grid text-start lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://github.com/Liveir"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
